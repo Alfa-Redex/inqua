f853c3305b5948bb8da2d639049557f0c7c4afef
Red Ash <redashrules@gmail.com>
2017-08-05 12:35:59 +0530

    Upl

---
+++ index.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html>
+  <meta name="viewport" content="width=device-width, initial-scale=1"  />
+  <link rel="stylesheet" href="ui/site.style.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="icon" href="ui/logo.png" />
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400');
+  </style>
+  <title>INQUA 2K17 | Ingenium Quadrivium</title>
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="main_back_cover">
+      <div class="container">
+        <video class="video_cover" src="ui/main_cover.mp4" autoplay loop width="100%">
+        </video>
+      </div>
+      <div class="social_icons_m">
+        <button ><i class="fa fa-facebook-f" onclick="window.location='https://www.facebook.com/techexpoINQUA17/?ref=br_rs'"></i></button>
+        <button><i class="fa fa-instagram" onclick="window.location='https://www.instagram.com/inqua_17/'"></i></button>
+        <button><i class="fa fa-youtube-play" onclick="window.location='https://www.youtube.com/channel/UCuPM9cDroz5c5vGG4TL6dhQ'"></i></button>
+      </div>
+      <div class="main_cover_inner_contents">
+        <div class="mc_ic">
+          <div class="main_inner_wrapper">
+            <div class="logo_main">
+              <img src="ui/logo.png" draggable="false"/>
+            </div>
+            <div class="logo_text">
+              <img src="ui/inqua.png" draggable="false" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="brouchure_btn">
+        <button class="bro_btn" onclick="window.location='https://drive.google.com/open?id=0B4yTtqQXSymzTFFIN3lZMzlEVVU'" target="_blank">Get Brouchure</button>
+      </div>
+    </div>
+    <div class="content_two">
+
+      <div class="inner_text_1">
+        <h2>What's Inqua '17 ?</h2>
+        <p>
+          INQUA'17 is the Inter College Technical Expo intended to hone the technical skills of students of our state. INQUA is the abbreviation of latin words Ingenium and Quadrivium which means intelligence or talent coming through four roads converging at a point.The events of the expo are so planned that in the process of exercising their academic intellects,the participants come up with innovations and solutions for the world to take notice of INQUA'17. INQUA'17 is set to astonish the people as we bring them the technologies on which billions of dollars are invested and on which future lies.Wait for the unbridled technology, creative minds and astonish nights.<br />
+          Get ready for INQUA...
+        </p>
+      </div>
+    </div>
+    <div class="content_three_inner">
+      <div class="inner_text_2">
+        <div class="icon_text_1">
+          <i class="fa fa-calendar" ></i>
+        </div>
+        <div class="text_inner_three">
+          <p>
+            <h4>Events</h4>
+            Extraordinary events is the basic motto of a successful Tech-Expo. INQUA conquers the next dimensions of Technology and Gaming from the different engineering departments. Competitions of INQUA bears interesting prize money and long lasting experience. If you think you can win, come be the part of technical fights.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="content_three_inner">
+      <div class="inner_text_2">
+        <div class="icon_text_1">
+          <i class="fa fa-film" ></i>
+        </div>
+        <div class="text_inner_three">
+          <p>
+            <h4>Celluloid</h4>
+            Film Recognizes neither time nor space, Only the limits of man's Imagination. INQUA organise a film fest accordance with the film club of CETkr Odessa. Its a variety of experience around a technical fest,
+            its gonna unfolds the artist in the Engineer. Met the people Behind the Screen of the hard works. Lets imagination expell around.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="content_three_inner">
+      <div class="inner_text_2">
+        <div class="icon_text_1">
+          <i class="fa fa-map-signs" ></i>
+        </div>
+        <div class="text_inner_three">
+          <p>
+            <h4>Exhibitions</h4>
+            Extraordinary events is the basic motto of a successful Tech-Expo. INQUA conquers the next dimensions of Technology and Gaming from the different engineering departments. Competitions of INQUA bears interesting prize money and long lasting experience. If you think you can win, come be the part of technical fights.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="content_three_inner">
+      <div class="inner_text_2">
+        <div class="icon_text_1">
+          <i class="fa fa-gears" ></i>
+        </div>
+        <div class="text_inner_three">
+          <p>
+            <h4>Workshops</h4>
+            Extraordinary events is the basic motto of a successful Tech-Expo. INQUA conquers the next dimensions of Technology and Gaming from the different engineering departments. Competitions of INQUA bears interesting prize money and long lasting experience. If you think you can win, come be the part of technical fights.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="content_three_inner">
+      <div class="inner_text_2">
+        <div class="icon_text_1">
+          <i class="fa fa-gift" ></i>
+        </div>
+        <div class="text_inner_three">
+          <p>
+            <h4>Gyaan</h4>
+            Knowledge Revolution INQUA brings a Techno Quizzing event which includes the younger minds of Kasargod and Kannur. Igniting Minds and Kindling Curiosities in the Ultimate Battle of Minds. Experience the battle of knowledge, behold till INQUA. The countdown begins...
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="specials_container" id="sl_j">
+      <div class="specials_inner">
+        <div class="special_head">
+          <h1>Interactive Gaming Sessions</h1>
+        </div>
+        <div class="specials_content">
+          <p>
+            INQUA three days, thousands of hands and you to conquer the next dimension, we bring to you an extra ordinary competition in all engineering departments. If you think you can win, come win against the competitors.
+          </p>
+          <img src="ui/vr.png" /><img src="ui/gp.png" />
+          <p class="caption_min">
+            * Hitman, Assassins Creed, Need for Speed and Watch Dogs are trademarks of Ubisoft, EA sports and IO interactive studios.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="specials_container_1">
+      <div class="specials_inner_fr">
+        <div class="special_head">
+          <br /><br />
+          <h1>Feier - Fly By The Night</h1>
+        </div>
+        <div class="specials_content" style="position:relative; top: 40px;">
+          <p>
+            <br /><br />
+            The nights of INQUA broke out with musical extravaganza with more than 1000 spectators. Prepare yourself to experience the proshows of INQUA which unfolds the dazzling performance of artists.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="contact_us_divider">
+    </div>
+
+    <div class="contact_us_main">
+      <div>
+        <h2 style="color:#fff; margin-top: 0; text-align: center; padding: 10px;">Contact Us</h2>
+      </div>
+      <div class="c_u_cont">
+        <div class="contact_list">
+          <div class="person_data">
+            <div class="usr_img">
+              <img src="ui/pranav.jpg" />
+            </div>
+            <div class="usr_data">
+              <li>
+                <strong>Convener</strong>
+              </li>
+              <li>
+                Pranav A V
+              </li>
+              <li title="Click / Tap to call">
+                <a href="tel: +919497850708"><i class="fa fa-phone"></i> 9497850708</a>
+              </li>
+              <li onclick="window.location='mailto:pranavp637@gmail.com'" style="cursor: pointer" title="Click to mail">
+                pranavp637@gmail.com
+              </li>
+              <li>
+                <button onclick="window.location='https://www.facebook.com/avpranavksd'"><i class="fa fa-facebook-official"></i></button><button><i class="fa fa-instagram"></i></button><button><i class="fa fa-twitter"></i></button>
+              </li>
+            </div>
+          </div>
+          <div class="person_data">
+            <div class="usr_img">
+              <img src="ui/aswin.jpg" />
+            </div>
+            <div class="usr_data">
+              <li>
+                <strong>Student Coordinator</strong>
+              </li>
+              <li>
+                Aswin P
+              </li>
+              <li title="Click / Tap to call">
+                <a href="tel: +918907312874 "><i class="fa fa-phone"></i> 8907312874 </a>
+              </li>
+              <li onclick="window.location='mailto:pranavp637@gmail.com'" style="cursor: pointer" title="Click to mail">
+                aswinpoduval94@gmail.com
+              </li>
+              <li>
+                <button onclick="window.location='https://www.facebook.com/aswinpoduval1616'"><i class="fa fa-facebook-official"></i></button><button><i class="fa fa-instagram"></i></button><button><i class="fa fa-twitter"></i></button>
+              </li>
+            </div>
+          </div>
+        </div>
+        <div class="contact_list">
+          <div class="person_data">
+            <div class="usr_img">
+              <img src="ui/bibin.jpg" />
+            </div>
+            <div class="usr_data">
+              <li>
+                <strong>Joint Convener</strong>
+              </li>
+              <li>
+                Bibin Joshi
+              </li>
+              <li title="Click / Tap to call">
+                <a href="tel: +919526965030"><i class="fa fa-phone"></i> 9526965030</a>
+              </li>
+              <li onclick="window.location='mailto:pranavp637@gmail.com'" style="cursor: pointer" title="Click to mail">
+                bjche619@gmail.com
+              </li>
+              <li>
+                <button><i class="fa fa-facebook-official" onclick="window.location='https://www.facebook.com/bibin.joshy.3'"></i></button><button><i class="fa fa-instagram"></i></button><button><i class="fa fa-twitter"></i></button>
+              </li>
+            </div>
+          </div>
+          <div class="person_data">
+            <div class="usr_img">
+              <img src="ui/aneesh.jpg" />
+            </div>
+            <div class="usr_data">
+              <li>
+                <strong>Staff Advisor</strong>
+              </li>
+              <li>
+                Aneesh Kumar
+              </li>
+              <li title="Click / Tap to call">
+                <a href="tel: +919400808443"><i class="fa fa-phone"></i> 9400808443</a>
+              </li>
+              <li onclick="window.location='mailto:pranavp637@gmail.com'" style="cursor: pointer" title="Click to mail">
+                aneeshkoorkara@gmail.com
+              </li>
+              <li>
+                <button><i class="fa fa-facebook-official" onclick="window.location='https://www.facebook.com/aneeshkoorkara.kumar'"></i></button><button><i class="fa fa-instagram"></i></button><button><i class="fa fa-twitter"></i></button>
+              </li>
+            </div>
+          </div>
+        </div>
+        <div class="contact_list">
+          <div class="person_data">
+            <div class="usr_img">
+              <img src="ui/vishnu.jpg" />
+            </div>
+            <div class="usr_data">
+              <li>
+                <strong>Web Designer</strong>
+              </li>
+              <li>
+                Vishnu Kalanad
+              </li>
+              <li title="Click / Tap to call">
+                <a href="tel: +919645064291"><i class="fa fa-phone"></i> 9645064291</a>
+              </li>
+              <li onclick="window.location='mailto:pranavp637@gmail.com'" style="cursor: pointer" title="Click to mail">
+                dxbvishnu@gmail.com
+              </li>
+              <li>
+                <button onclick="window.location='https://www.facebook.com/vishnukalanad'"><i class="fa fa-facebook-official"></i></button><button><i class="fa fa-instagram"></i></button><button><i class="fa fa-twitter"></i></button>
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container_ftr">
+        <div class="college_logo_cn">
+          <div class="logo_img_cn">
+            <img src="ui/cet.png" />
+          </div>
+          <div class="tkr-txt">
+            <p style="color: #fff">
+              Inspiring Innovative Ideas <br />
+              <strong>College of engineering Trikaripur</strong><br />
+              Under the Co-operative Academy of Professional Education<br />
+              Established by government of kerala
+            </p>
+          </div>
+        </div>
+        <div class="scl_lnk_bottom">
+          <p>
+            Conact Us :
+          </p>
+          <div class="social_icons_m">
+            <button ><i class="fa fa-facebook-f" onclick="window.location='https://www.facebook.com/techexpoINQUA17/?ref=br_rs'"></i></button>
+            <button><i class="fa fa-instagram" onclick="window.location='https://www.instagram.com/inqua_17/'"></i></button>
+            <button><i class="fa fa-youtube-play" onclick="window.location='https://www.youtube.com/channel/UCuPM9cDroz5c5vGG4TL6dhQ'"></i></button>
+            <button><i class="fa fa-envelope-o" onclick="window.location='mailto:inqua17@gmail.com'"></i></button>
+          </div>
+        </div>
+      </div>
+
+    </footer>
+  </body>
+</html>
